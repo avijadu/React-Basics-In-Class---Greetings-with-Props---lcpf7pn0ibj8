6760293a248a6dd0ca8c094e598e60ef4373359d
Avinash Patel <dha1883@gmail.com>
2022-03-01 17:30:10 +0530

    final commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react'
+
+function Welcome(props) {
+    return (
+        <div>
+            Hey! <h1>{props.name}</h1>
+            <h2>Welcome to Newton School.</h2>
+        </div>
+    )
+}
+
+export default Welcome
